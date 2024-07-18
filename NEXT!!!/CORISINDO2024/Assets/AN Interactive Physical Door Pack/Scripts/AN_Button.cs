8c2edfea79a6a78fe7bc5fd26414e5a1f2fb39c8
Gawira <149558045+Gawira@users.noreply.github.com>
2024-07-18 16:59:28 +0700

    leveranimationdone

--- NEXT!!!/CORISINDO2024/Assets/AN Interactive Physical Door Pack/Scripts/AN_Button.cs
+++ NEXT!!!/CORISINDO2024/Assets/AN Interactive Physical Door Pack/Scripts/AN_Button.cs
@@ -50,51 +50,49 @@ public class AN_Button : MonoBehaviour
     {
         if (!Locked)
         {
-            if (Input.GetKeyDown(KeyCode.E) && !isValve && DoorObject != null && DoorObject.Remote && NearView()) // 1.lever and 2.button
+            if (Input.GetKeyDown(KeyCode.E) && !isValve &&  NearView()) // 1.lever and 2.button
             {
-                DoorObject.Action(); // void in door script to open/close
                 if (isLever) // animations
                 {
-                    if (DoorObject.isOpened) anim.SetBool("LeverUp", true);
-                    else anim.SetBool("LeverUp", false);
+                    anim.SetBool("LeverUp", true);  
                 }
                 else anim.SetTrigger("ButtonPress");
             }
-            else if (isValve && RampObject != null) // 3.valve
-            {
-                // changing value in script
-                if (Input.GetKey(KeyCode.E) && NearView())
-                {
-                    if (valveBool)
-                    {
-                        if (!isOpened && CanOpen && current < max) current += speed * Time.deltaTime;
-                        if (isOpened && CanClose && current > min) current -= speed * Time.deltaTime;
+            // else if (isValve && RampObject != null) // 3.valve
+            // {
+            //     // changing value in script
+            //     if (Input.GetKey(KeyCode.E) && NearView())
+            //     {
+            //         if (valveBool)
+            //         {
+            //             if (!isOpened && CanOpen && current < max) current += speed * Time.deltaTime;
+            //             if (isOpened && CanClose && current > min) current -= speed * Time.deltaTime;
 
-                        if (current >= max)
-                        {
-                            isOpened = true;
-                            valveBool = false;
-                        }
-                        else if (current <= min)
-                        {
-                            isOpened = false;
-                            valveBool = false;
-                        }
-                    }
+            //             if (current >= max)
+            //             {
+            //                 isOpened = true;
+            //                 valveBool = false;
+            //             }
+            //             else if (current <= min)
+            //             {
+            //                 isOpened = false;
+            //                 valveBool = false;
+            //             }
+            //         }
 
-                }
-                else
-                {
-                    if (!isOpened && current > min) current -= speed * Time.deltaTime;
-                    if (isOpened && current < max) current += speed * Time.deltaTime;
-                    valveBool = true;
-                }
+            //     }
+            //     else
+            //     {
+            //         if (!isOpened && current > min) current -= speed * Time.deltaTime;
+            //         if (isOpened && current < max) current += speed * Time.deltaTime;
+            //         valveBool = true;
+            //     }
 
-                // using value on object
-                transform.rotation = startQuat * Quaternion.Euler(0f, 0f, current * ValveSpeed);
-                if (xRotation) RampObject.rotation = rampQuat * Quaternion.Euler(current, 0f, 0f); // I have a doubt in working correctly
-                else if (yPosition) RampObject.position = new Vector3(RampObject.position.x, startYPosition + current, RampObject.position.z);
-            }
+            //     // using value on object
+            //     transform.rotation = startQuat * Quaternion.Euler(0f, 0f, current * ValveSpeed);
+            //     if (xRotation) RampObject.rotation = rampQuat * Quaternion.Euler(current, 0f, 0f); // I have a doubt in working correctly
+            //     else if (yPosition) RampObject.position = new Vector3(RampObject.position.x, startYPosition + current, RampObject.position.z);
+            // }
         }
     }
 
