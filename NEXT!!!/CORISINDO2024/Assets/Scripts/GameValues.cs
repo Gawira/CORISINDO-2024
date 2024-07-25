6ebe96568b0cb5b7f72c5eecf4546e6b898d4cdb
Gawira <149558045+Gawira@users.noreply.github.com>
2024-07-25 15:11:33 +0700

    gamevalues initialiazed

---
+++ NEXT!!!/CORISINDO2024/Assets/Scripts/GameValues.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameValues : MonoBehaviour
+{
+    public float timerDuration = 360f; // 6 minutes in seconds
+    public string dayTransitionSceneName = "Day Transition";
+    public int startingMoney = 50000;
+
+    private float timer;
+    private int money;
+
+    void Start()
+    {
+        timer = timerDuration;
+        money = startingMoney;
+    }
+
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                ChangeScene();
+            }
+        }
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(dayTransitionSceneName);
+    }
+
+    public int GetMoney()
+    {
+        return money;
+    }
+
+    public void AddMoney(int amount)
+    {
+        money += amount;
+    }
+
+    public void SubtractMoney(int amount)
+    {
+        money -= amount;
+    }
+
+    public float GetRemainingTime()
+    {
+        return timer;
+    }
+}
