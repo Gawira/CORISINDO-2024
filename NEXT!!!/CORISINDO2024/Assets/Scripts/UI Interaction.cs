133626ec000f73a1de78884ec7c866b078879376
Gawira <149558045+Gawira@users.noreply.github.com>
2024-07-27 10:24:29 +0700

    how to play

--- NEXT!!!/CORISINDO2024/Assets/Scripts/UI Interaction.cs
+++ NEXT!!!/CORISINDO2024/Assets/Scripts/UI Interaction.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class UIInteraction : MonoBehaviour
 {
     public GameObject popupSetting;  // Referensi ke UI Popup
+    public GameObject popupSetting1;
+    public GameObject popupSetting2;
 
     // Fungsi untuk menampilkan popup
     public void ShowPopup()
     {
         popupSetting.SetActive(true);
+        popupSetting1.SetActive(true);
+        popupSetting2.SetActive(true);
     }
 
     // Fungsi untuk menyembunyikan popup
     public void HidePopup()
     {
         popupSetting.SetActive(false);
+        popupSetting1.SetActive(false);
+        popupSetting2.SetActive(false);
     }
 
     // Fungsi untuk toggle popup
